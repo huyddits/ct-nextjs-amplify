1a0d4444d3bb6a65172124ff957ad37bde00edc8
Đàm Đình Huy <huydd@iceteasoftware.com>
2025-05-14 10:52:16 +0700

    fix(setup): build

---
+++ app/(auth)/login/twitter-callback/TwitterCallbackPage.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+import { AppLoadingFullScreen } from '@/components/compose';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useAuthStore } from '@/store';
+
+export default function TwitterCallbackPage() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const { setToken } = useAuthStore();
+
+  useEffect(() => {
+    const token = searchParams.get('accessToken') as string;
+
+    if (token) {
+      setToken(token);
+      router.replace('/home');
+    } else {
+      setToken(null);
+      router.replace('/login');
+    }
+  }, [searchParams]);
+
+  return <AppLoadingFullScreen />;
+}
