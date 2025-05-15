2eb09fbc956c67922f12f48f7d18eec217179c64
Đàm Đình Huy <huydd@iceteasoftware.com>
2025-05-15 18:36:36 +0700

    fix(component): conflict

--- utils/constants/routes.ts
+++ utils/constants/routes.ts
@@ -21,8 +21,4 @@ export const ROUTES = {
   HIT_MISS_DATA: 'hit-miss/data',
 };
 
-<<<<<<< HEAD
-export const WHITE_LIST = [`/${ROUTES.TERMS_AND_CONDITIONS}`, `/${ROUTES.ABOUT_US}`];
-=======
 export const WHITE_LIST = [`${ROUTES.TERMS_AND_CONDITIONS}`, `/${ROUTES.ABOUT_US}`];
->>>>>>> 401cbbb (fix(component): fix feedback)
