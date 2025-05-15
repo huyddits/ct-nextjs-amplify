7d0a01892ea72f030bffee617775db58dd2dcf2e
Nguyen Minh Duc <ducnm@iceteasoftware.com>
2025-05-15 10:53:47 +0000

    Feat/hit miss ui

--- app/(in-app)/measurement/layout.tsx
+++ app/(in-app)/measurement/layout.tsx
@@ -5,11 +5,7 @@ export default function MeasurementLayout({ children }: { children: React.ReactN
     <div>
       <TabNavigationMeasurement />
       <div>{children}</div>
-<<<<<<< HEAD
       <div className="h-16" />
-=======
-      <div className="h-32" />
->>>>>>> c7c5033 (fix(component): move html to this file instead of export from _components,...)
     </div>
   );
 }
