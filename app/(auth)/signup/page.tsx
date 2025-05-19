ddc7abbd35d39ac16b359bd89d7a89de631de023
Nguyen Minh Duc <ducnm@iceteasoftware.com>
2025-05-19 04:47:45 +0000

    fix(component): fix ui for forgot password and reset password

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { LogoSection, SignupForm, LoginLinkSection } from "./_components";
-import { FooterSection } from "../_components";
+import React from 'react';
+import { SignupForm, LoginLinkSection } from './_components';
+import { LogoSection, FooterSection } from '../_components';
 
 export const metadata = {
-  title: "Cheer Trainer | Signup",
-  description: "Create your account to join Cheer Trainer today.",
+  title: 'Cheer Trainer | Signup',
+  description: 'Create your account to join Cheer Trainer today.',
 };
 
 export default function SignupPage() {
   return (
     <React.Fragment>
-      <LogoSection />
+      <LogoSection description="Create your account" />
       <SignupForm />
       <LoginLinkSection />
       <FooterSection />
