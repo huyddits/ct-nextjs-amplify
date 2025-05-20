cce341f6be084e86e2633818b23f67ed9baeb597
chuyenmq <quocchuyentq@gmail.com>
2025-05-20 10:54:54 +0700

    feat(component): add pastTraningCardio

--- app/(in-app)/training/cardio/past/page.tsx
+++ app/(in-app)/training/cardio/past/page.tsx
@@ -1,11 +1,20 @@
-import { PastTraining, DatePastTrainingSection } from '../_components';
+import {
+  PastTraining,
+  DatePastTrainingSection,
+  PerformanceMetrics,
+  WeeklySummarySection,
+  WeeklyWorkoutsSection,
+} from './_components';
 
 export default function PastTrainingPage() {
   return (
-    <div>
+    <div className="pt-[56px] pb-[80px] max-w-3xl mx-auto px-4">
       <PastTraining />
       <main>
         <DatePastTrainingSection />
+        <PerformanceMetrics />
+        <WeeklySummarySection />
+        <WeeklyWorkoutsSection />
       </main>
     </div>
   );
