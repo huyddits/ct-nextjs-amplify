4e7aed422902be0e5ebbad70ebf5840e915f6b33
Nghiem Xuan Thang <thangnx@iceteasoftware.com>
2025-05-29 06:47:29 +0000

    fix(component): only call this type of API common data once

--- store/useCardio.store.ts
+++ store/useCardio.store.ts
@@ -1,13 +1,19 @@
 // src/store/useCardioStore.ts
+'use client';
 import { create } from 'zustand';
+import { SelectOption } from '@/components/compose/AppSelect.client';
 
 type Interval = {
   duration?: string;
   rpe?: string;
   distance?: string;
-  distance_unit?: string;
+  distanceUnit?: string;
   heartRateMin?: string;
   heartRateMax?: string;
+};
+
+type ExerciseOption = SelectOption & {
+  units: SelectOption[];
 };
 
 type CardioStore = {
@@ -15,11 +21,19 @@ type CardioStore = {
   setIntervalsList: (list: Interval[]) => void;
   addInterval: (interval: Interval) => void;
   clearCardioIntervals: () => void;
+  exerciseOptions: ExerciseOption[];
+  setExerciseOptions: (options: ExerciseOption[]) => void;
+  rpeOptions: SelectOption[];
+  setRpeOptions: (options: SelectOption[]) => void;
 };
 
-export const useIntervalsCardioStore = create<CardioStore>(set => ({
+export const useCardioStore = create<CardioStore>(set => ({
   intervalsList: [],
   setIntervalsList: list => set({ intervalsList: list }),
   addInterval: interval => set(state => ({ intervalsList: [...state.intervalsList, interval] })),
   clearCardioIntervals: () => set({ intervalsList: [] }),
+  exerciseOptions: [],
+  setExerciseOptions: options => set({ exerciseOptions: options }),
+  rpeOptions: [],
+  setRpeOptions: options => set({ rpeOptions: options }),
 }));
