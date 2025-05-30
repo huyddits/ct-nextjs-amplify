009371025cf26c6751fc87aa4e3e99aa72e920dc
Nguyen Minh Duc <ducnm@iceteasoftware.com>
2025-05-30 06:28:51 +0000

    Fix(component): replace logo in welcome page

--- app/(auth)/_components/LogoSection.tsx
+++ app/(auth)/_components/LogoSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Logo } from '@/app/_components';
 interface LogoSectionProps {
   description: string;
 }
@@ -6,12 +7,7 @@ interface LogoSectionProps {
 export default function LogoSection(props: LogoSectionProps) {
   return (
     <div className="text-center mb-8">
-      {/* <div className="h-16 w-16 bg-primary rounded-full flex items-center justify-center mx-auto mb-4">
-        <span className="text-white text-2xl font-bold">CT</span>
-      </div> */}
-      <div className="h-16 w-16 bg-primary rounded-full flex items-center justify-center mx-auto mb-4">
-        <img src={'/logo.jpg'} alt="logo source" />
-      </div>
+      <Logo />
       <h1 className="text-2xl font-bold text-primary">Cheer Trainer</h1>
       <p className="text-gray-600 mt-2">{props.description}</p>
     </div>
