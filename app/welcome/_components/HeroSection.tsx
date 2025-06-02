eda61e7bcf6ae36ba09296ec6f83ddd071ffbb43
--local <--local>
2025-06-02 14:38:30 +0700

    fix(component): fix bug welcome screen logo and edit logo about-us screen

--- app/welcome/_components/HeroSection.tsx
+++ app/welcome/_components/HeroSection.tsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
         <div className="absolute inset-0 bg-gradient-to-b from-(--primary)/90 to-(--primary)/70 z-10" />
         <div className="relative h-[50vh] min-h-[400px] bg-cover bg-center">
           <div className="absolute inset-0 flex flex-col items-center justify-center z-20 text-white p-6 text-center">
-            <Logo className="overflow-hidden" />
+            <Logo className="rounded-xl overflow-hidden" />
 
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
               Welcome to Cheer Trainer
