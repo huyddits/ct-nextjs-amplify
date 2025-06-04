e664aaff732a060a07690b616a1d5044cb561361
--local <--local>
2025-06-04 18:16:46 +0700

    fix(component): fix comment code strength

--- app/(in-app)/training/strength/new/_components/AddExercisesProgramSection.tsx
+++ app/(in-app)/training/strength/new/_components/AddExercisesProgramSection.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import ExercisesProgramItem from './ExercisesProgramitem';
+import { MinusIcon } from 'lucide-react';
 
 const initialExercises = [
   { name: 'Barbell Squat', description: 'Lower Body Strength' },
@@ -24,26 +25,9 @@ export default function ExercisesProgramList() {
             <div className="flex items-center w-full">
               <ExercisesProgramItem name={name} description={description} />
             </div>
-            <button
-              onClick={() => handleRemove(index)}
-              className="text-red-600 p-2"
-              aria-label={`Remove ${name}`}
-            >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5 12H19"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+            <button onClick={() => handleRemove(index)} className="text-green-800 p-2">
+              <span className="sr-only">Remove {name}</span>
+              <MinusIcon size={24} strokeWidth={2} />
             </button>
           </div>
         ))}
--- app/(in-app)/training/strength/pastStrength/_components/DatePastStrengthSection.tsx
+++ app/(in-app)/training/strength/pastStrength/_components/DatePastStrengthSection.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import AppCalendar from '@/components/compose/AppCalendar.client';
 import { Button } from '@/components/ui/button';
-import { ChevronLeft, ChevronRight, Calendar as CalendarIcon } from 'lucide-react';
+import { ChevronLeftIcon, ChevronRightIcon, Calendar as CalendarIcon } from 'lucide-react';
 import { startOfWeek, endOfWeek, addWeeks, subWeeks, format } from 'date-fns';
 
 export default function DatePastStrengthSection({
@@ -17,7 +17,8 @@ export default function DatePastStrengthSection({
 
   const previousWeek = () => onChangeDate(subWeeks(selectedDate, 1));
   const nextWeek = () => onChangeDate(addWeeks(selectedDate, 1));
-  const formatDateRange = () => {
+
+  const formattedDateRange = useMemo(() => {
     const start = startOfWeek(selectedDate, { weekStartsOn: 1 });
     const end = endOfWeek(selectedDate, { weekStartsOn: 1 });
 
@@ -26,19 +27,19 @@ export default function DatePastStrengthSection({
     }
 
     return `${format(start, 'MMMM d')} - ${format(end, 'd, yyyy')}`;
-  };
+  }, [selectedDate]);
 
   return (
     <div className="flex items-center mb-4 w-full">
       <Button variant="outline" size="icon" onClick={previousWeek} className="mr-1">
-        <ChevronLeft className="h-4 w-4" />
+        <ChevronLeftIcon className="h-4 w-4" />
       </Button>
 
       <AppCalendar
         icon={<CalendarIcon className="h-4 w-4" />}
         label=""
         value={highlightedDate ?? selectedDate}
-        triggerLabel={formatDateRange()}
+        triggerLabel={formattedDateRange}
         dateFormat="DD/MM/YYYY"
         onChange={date => {
           if (date) {
@@ -50,7 +51,7 @@ export default function DatePastStrengthSection({
       />
 
       <Button variant="outline" size="icon" onClick={nextWeek} className="ml-1">
-        <ChevronRight className="h-4 w-4" />
+        <ChevronRightIcon className="h-4 w-4" />
       </Button>
     </div>
   );
