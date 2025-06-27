6e50965842cc584101e8b0e1f61d6f7c5caff41a
james <huytn@iceteasoftware.com>
2025-06-27 15:14:24 +0700

    feat(component): hitmiss

--- app/(in-app)/hit-miss/_components/TabNavigationHitMiss.client.tsx
+++ app/(in-app)/hit-miss/_components/TabNavigationHitMiss.client.tsx
@@ -2,10 +2,26 @@
 import { TabNavigation } from '@/app/(in-app)/_components';
 import { usePathname } from 'next/navigation';
 import { ROUTES } from '@/utils/constants';
+import { useRole } from '@/hooks';
 
 export default function TabNavigationHitMiss() {
   const pathname = usePathname();
   const [_, __, subPath] = pathname.split('/');
+  const { isCoach } = useRole();
+  if (!isCoach)
+    return (
+      <TabNavigation
+        listTabs={[
+          {
+            title: 'Hit/Miss',
+            value: 'data',
+          },
+        ]}
+        value={subPath}
+        prefixPath={`/${ROUTES.HIT_MISS}`}
+        className="fixed w-full z-20 shadow top-0"
+      />
+    );
   return (
     <TabNavigation
       listTabs={[
--- app/(in-app)/hit-miss/page.tsx
+++ app/(in-app)/hit-miss/page.tsx
@@ -1,6 +1,9 @@
+'use client';
+import { useRole } from '@/hooks';
 import { ROUTES } from '@/utils/constants';
 import { redirect } from 'next/navigation';
 
 export default function MeasurementPage() {
-  return redirect(`/${ROUTES.HIT_MISS_ROUTINES}`);
+  const { isCoach } = useRole();
+  return isCoach ? redirect(`/${ROUTES.HIT_MISS_ROUTINES}`) : redirect(`/${ROUTES.HIT_MISS_DATA}`);
 }
